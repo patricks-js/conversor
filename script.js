c6ce420c13e692272bf1a7db273a2ea8f30bec50
Patrick-home <lucas.patrick.lsilva@gmail.com>
2021-10-28 15:29:35 -0300

    interface melhorada

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ let real = document.querySelector(`li#real`)
 let valConv = document.querySelector(`input#valConv`)
 let calc = document.querySelector(`button#calc`)
 let label = document.querySelector(`label#label`)
+let cot = document.querySelector(`input#cot`)
 
 celsius.addEventListener(`click`, calcCelsius)
 fahrenheit.addEventListener(`click`, calcFahren)
 real.addEventListener(`click`, calcReal)
 
 function calcCelsius(){
+    // valConv = 
+    res.textContent = ``
+    cot.style = `display: none;`
     // let paragraph = document.createElement(`p`)
     // res.appendChild(paragraph)
     // paragraph.innerHTML = `Celsius para Fahrenheit`
@@ -19,11 +23,14 @@ function calcCelsius(){
     calc.disabled = false
     calc.addEventListener(`click`, calcular)
     function calcular() {
-        res.innerHTML = `O resultado em Fahrenheit é: ${(9 * valConv + 160) / 5}`
+        let result = ((9 * Number(valConv.value) + 160) / 5)
+        res.innerHTML = `O resultado em Fahrenheit é: ${result.toFixed(2)} °F`
     }
 }
 
 function calcFahren() {
+    res.textContent = ``
+    cot.style = `display: none;`
     // let paragraph = document.createElement(`p`)
     // res.appendChild(paragraph)
     // paragraph.innerHTML = `Fahrenheit para Celsius`
@@ -32,39 +39,25 @@ function calcFahren() {
     calc.disabled = false
     calc.addEventListener(`click`, calcular)
     function calcular() {
-        res.innerHTML = `O resultado em Celsius é: ${((valConv - 32) * 5) / 9}`
+        let result = (((Number(valConv.value) - 32) * 5) / 9)
+        res.innerHTML = `O resultado em Celsius é: ${result.toFixed(2)} °C`
     }
 }
 
 function calcReal() {
+    res.textContent = ``
     // let paragraph = document.createElement(`p`)
     // res.appendChild(paragraph)
     // paragraph.textContent = `Real para dólar`
-    let cot = document.querySelector(`input#cot`)
     cot.style = `display: block;`
 
     valConv.disabled = false
     label.textContent = `Digite o valor em Real`
     calc.disabled = false
     calc.addEventListener(`click`, calcular)
+    // let result = Number(valConv.value) / Number(cot.value)
     function calcular() {
-        res.innerHTML = `O valor em Dólar é: ${Math.floor(valConv.value) / cot.value}`
+        let result = Number(valConv.value) / Number(cot.value)
+        res.innerHTML = `<O valor em Dólar é: U$ ${result.toFixed(2)}`
     }
 }
-
-
-// 
-//     let input = document.createElement(`input`)
-//     let button = document.createElement(`button`)
-//     let rest = document.createElement(`button`)
-
-//     res.appendChild(input)
-//     res.appendChild(button)
-//     res.appendChild(rest)
-//     button.textContent = `Calcular`
-//     rest.textContent = `Resetar`
-//     rest.addEventListener(`click`, restart)
-//     function restart() {
-//         res.reset()
-//     }
-// 
